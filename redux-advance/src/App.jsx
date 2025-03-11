22f4c9a6e449ce5caab5786bb84f8df89e3f96cb
shivakumar2006 <official.shivakumar06@gmail.com>
2025-03-11 13:41:09 +0530

    commit

---
+++ redux-advance/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className='w-screen h-full flex justify-center items-center bg-black text-white'>
+            App
+        </div>
+    )
+}
+
+export default App;
---
+++ redux-advance/src/features/slices/slice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+const slice = createSlice({
+    initialState,
+    reducers: [
+         
+    ]
+})
