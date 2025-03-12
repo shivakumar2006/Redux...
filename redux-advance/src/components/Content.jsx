9bf2c9d5d37b83af3607a303fff24e1f36a7133d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-03-12 20:02:05 +0530

    commit

--- redux-advance/src/components/Content.jsx
+++ redux-advance/src/components/Content.jsx
@@ -1,6 +1,28 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
+import { useDispatch } from "react-redux";
 
 const Content = () => {
+
+    // const [ selectGender, setSelectGender ] = useState(null);
+
+    // const handleGenderSelect = (gender) => {
+    //     setSelectGender(prevGender => (prevGender === gender ? null : gender));
+    // } 
+
+    const [ users, setUsers ] = useState();
+
+    const dispatch = useDispatch();
+
+    const getUserData = (e) => {
+        setUsers({...users, [e.target.name] : e.target.value})
+
+        console.log(users);
+    }
+
+    const handleSubmit = () => {
+        dispatch()
+    }
+
     return (
         <div className='w-screen h-screen bg-primary py-10'>
             <div className='flex justify-center items-center'>
@@ -8,8 +30,10 @@ const Content = () => {
             </div>
             <input 
                 type='text'
+                name="name"
                 className='w-150 h-10 border-white flex justify-center items-center ml-106 px-5 rounded bg-white'
-                placeholder='Enter Name...'
+                placeholder='Enter name...'
+                onChange={getUserData}
             />
 
             <div className='flex justify-center items-center mt-10'>
@@ -17,8 +41,10 @@ const Content = () => {
             </div>
             <input 
                 type='text'
+                name="email"
                 className='w-150 h-10 border-white flex justify-center items-center ml-106 px-5 rounded bg-white'
-                placeholder='Enter E-mail...'
+                placeholder='Enter e-mail...'
+                onChange={getUserData}
             />
 
             <div className='flex justify-center items-center mt-10'>
@@ -26,11 +52,37 @@ const Content = () => {
             </div>
             <input 
                 type='text'
+                name="age"
                 className='w-150 h-10 border-white flex justify-center items-center ml-106 px-5 rounded bg-white'
-                placeholder='Enter Age...'
+                placeholder='Enter age...'
+                onChange={getUserData}
             />
 
-            <select>Male</select>
+            <div className='flex justify-center items-center mt-5'>
+                <input
+                    className='cursor-pointer'
+                    type='radio' 
+                    name="gender"
+                    value="Male"
+                />
+                <label className="ml-4">Male</label>
+
+                <input
+                    className='ml-5 cursor-pointer' 
+                    type='radio' 
+                    name='gender'
+                    value="Female"
+                />
+                <label className="ml-4">Female</label>
+            </div>
+            <div className='flex justify-center items-center mt-8'>
+                <button 
+                    className='w-20 h-10 bg-blue-700 border-0 text-white rounded hover:bg-blue-600 cursor-pointer'
+                    onSubmit={handleSubmit}
+                > 
+                    Submit 
+                </button>
+            </div>
         </div>
     )
 }
--- redux-advance/src/features/slices/slice.js
+++ redux-advance/src/features/slices/slice.js
@@ -1,8 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit"; 
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"; 
 
-const slice = createSlice({
-    initialState,
-    reducers: [
-         
-    ]
+export const createUser = createAsyncThunk("createUser", async (data) => {
+    const response = await fetch("https://67d047e1825945773eb04baa.mockapi.io/crud", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+    try {
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        
+    }
 })
+
+export const userDetail = createSlice({
+    name: "userDetail",
+    initialState: {
+        users: [],
+        loading: false,
+        error: null,
+    }
+})
+
+export default userDetail.reducer;
